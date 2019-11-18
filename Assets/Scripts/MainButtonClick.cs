a10e6efd891068c2cc2af8928592386309883bb9
ramkillnani <54158723+ramkillnani@users.noreply.github.com>
2019-11-18 14:11:49 +1100

    Add files via upload
    2D Cookie Clicker project for the 2D clicker assignment.

---
+++ Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour
+{
+    public GameObject textBox;
+    public AudioSource cookieSound;
+
+    public void ClickTheButton()
+    {
+        cookieSound.Play();
+        GlobalCookies.cookieCount += 1;
+    }
+}
---
+++ Assets/Scripts/SellCookie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellCookie : MonoBehaviour
+{
+    public GameObject textBox;
+    public GameObject statusBox;
+    public AudioSource cashOne;
+    public AudioSource cashTwo;
+    public int generateTone;
+    public AudioSource noCookie;
+
+    public void ClickTheButton()
+    {
+        generateTone = Random.Range(1, 3);
+        if (GlobalCookies.cookieCount == 0)
+        {
+            noCookie.Play();
+            statusBox.GetComponent<Text>().text = "Not enough Cookies to sell";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            if (generateTone == 1)
+            {
+                cashOne.Play();
+            }
+            if (generateTone == 2)
+            {
+                cashTwo.Play();
+            }
+
+            GlobalCookies.cookieCount -= 1;
+            GlobalCash.cashCount += 1;
+        }
+    }
+}
